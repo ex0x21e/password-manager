a2ca3fbeb914b902c9e3bcd91e1bcb4d8d1746e6
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-04-02 14:21:00 +1200

    get password

--- main.go
+++ main.go
@@ -75,15 +75,13 @@ func (pm *PasswordManager)SavePassword(name, value, category string) error{
 //получение пароля
 func (pm *PasswordManager)GetPassword(name string) (Password, error){
 	if !pm.isInitialized{
-		return NewPassword("","",""),
-		fmt.Errorf("password manager does noi initialized")
+		return NewPassword("","",""), fmt.Errorf("password manager does noi initialized")
 	}
 
 	if password, ok := pm.passwords[name];ok{
 		return NewPassword(name, password.Value, password.Category), nil
 	}else{
-		return NewPassword("","",""),
-		fmt.Errorf("password not found")
+		return NewPassword("","",""), fmt.Errorf("password not found")
 	}
 }
 
